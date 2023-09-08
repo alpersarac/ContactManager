f6c617e54f51c14d0090fc32115b8bd15e22c9ac
alpersarac <alper.sarac42@gmail.com>
2023-09-09 02:47:59 +0300

    Token Authorization, Update method change with ContactDTO

--- ContactManager.API/Program.cs
+++ ContactManager.API/Program.cs
@@ -1,8 +1,10 @@
 using ContactManager.DataAccess.Data;
 using ContactManager.DataAccess.Repository;
 using ContactManager.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,11 +15,32 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+//DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+//Unit of work dependency injection
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+//JWT implementation
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = "YourIssuer", // Replace with your issuer
+        ValidateAudience = true,
+        ValidAudience = "YourAudience", // Replace with your audience
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("YourSecretKey")) // Replace with your secret key
+    };
+});
+
+
 
 var app = builder.Build();
 
--- ContactManager.DataAccess/Repository/IRepository/IRepository.cs
+++ ContactManager.DataAccess/Repository/IRepository/IRepository.cs
@@ -10,8 +10,8 @@ namespace ContactManager.DataAccess.Repository.IRepository
     public interface IRepository<T> where T : class
     {
         Task Create(T obj);
-        Task Update(T obj);
-        Task Delete(int id);
+        
+        Task<bool> Delete(int id);
         Task<T> GetById(int id);
         Task<List<T>> GetAll();
     }
